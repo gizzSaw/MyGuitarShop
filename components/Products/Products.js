0214a4f2f2dd67956c39861c116dde8f680ebe60
gizzSaw <gizzsaw@gmail.com>
2023-09-08 15:56:31 +0300

    counter

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,18 +1,51 @@
 class Products {
+    constructor() {
+        this.classNameActive = 'products-element__btn_active'
+        this.labelAdd = 'Добавить в корзину'
+        this.labelRemove = 'Удалить из корзины'
+    }
+
+    handleSetLocationStorage(element, id) {
+        const  { pushProduct, products } = localStorageUtil.putProducts(id)
+
+        if(pushProduct) {
+            element.classList.add(this.classNameActive)
+            element.innerHTML = this.labelRemove
+        } else {
+            element.classList.remove(this.classNameActive)
+            element.innerHTML = this.labelRemove
+        }
+        headerPage.render(products.length) // изменение счётчика header
+    }
+
+
     render() {
+        const productsStore = localStorageUtil.getProducts()
         let htmlCatalog = ''
 
         CATALOG.forEach(({ id, name, price, img }) => {
-            
+            let activeClass = ''
+            let activeText = ''
+
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd
+            } else {
+                activeText = ' '+this.labelRemove
+                activeClass = ' products-element__price_active'
+            }
+
+
 
             htmlCatalog += `
                 <li class="products-element">
                     <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}" />
-                    <span class="products-element__price">
+                    <span class="products-element__price${activeClass}">
                         ⚡️${price.toLocaleString()} USD
                     </span>
-                    <button class="products-element__btn">Добавить в корзину</button>
+                    <button class="products-element__btn" onClick="productsPage.handleSetLocationStorage(this, '${id}')">
+                        ${activeText}
+                    </button>
                 </li>
             `
         })
